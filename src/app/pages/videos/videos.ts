ee336218557ab6fa8bf9f0032c2e60061da5d87c
Chris P. <mary.pamis@gmail.com>
2025-12-11 14:39:12 -0600

    feat: Integrate video rohi.mp4

--- src/app/pages/videos/videos.ts
+++ src/app/pages/videos/videos.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-videos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './videos.html',
   styleUrl: './videos.scss',
 })
 export class VideosComponent {
-
+  videos = [
+    {
+      src: '/assets/rohi.mp4',
+      title: 'Chris in Action',
+      description: 'A video showcasing my skills and projects.'
+    }
+  ];
 }
