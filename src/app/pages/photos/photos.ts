039a7cc5e31be1c23b00c909e4d2fb550fb016bc
Chris P. <mary.pamis@gmail.com>
2025-12-11 14:01:52 -0600

    feat: Integrate real photos from assets

--- src/app/pages/photos/photos.ts
+++ src/app/pages/photos/photos.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-photos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './photos.html',
   styleUrl: './photos.scss',
 })
 export class PhotosComponent {
-
+  photos = [
+    { src: 'assets/IMG_1037.jpeg', caption: 'Chris Rohan' },
+    { src: 'assets/IMG_1335.jpeg', caption: 'Gallery Image' },
+    { src: 'assets/IMG_3111.jpeg', caption: 'Gallery Image' }
+  ];
 }
