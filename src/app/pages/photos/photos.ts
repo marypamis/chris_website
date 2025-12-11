329596601e7d3919ca2416c5203b5f38321fedea
Chris P. <mary.pamis@gmail.com>
2025-12-11 14:25:58 -0600

    fix: Set 2nd image position to top to prevent head cropping

--- src/app/pages/photos/photos.ts
+++ src/app/pages/photos/photos.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 export class PhotosComponent {
   photos = [
     { src: '/assets/IMG_1037.jpeg', caption: 'Chris Rohan', position: 'top' },
-    { src: '/assets/IMG_1335.jpeg', caption: 'Gallery Image', position: 'center' },
+    { src: '/assets/IMG_1335.jpeg', caption: 'Gallery Image', position: 'top' },
     { src: '/assets/IMG_3111.jpeg', caption: 'Gallery Image', position: 'top' }
   ];
 }
